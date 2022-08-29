97475d6f8b51c3c11da1c2c98e8a501daffbb522
Jester <justjestervz@gmail.com>
2022-08-29 18:21:46 +0300

    first commit

---
+++ app/src/main/java/com/example/planechat/presentation/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.planechat.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planechat.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
